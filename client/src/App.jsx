8e1afc0b13ac67dab7a8f7929e8bdd6747d615de
hafayouness <155436001+hafayouness@users.noreply.github.com>
2024-06-07 16:59:42 +0200

    new projet mern-blog

---
+++ client/src/App.jsx
@@ -0,0 +1,24 @@
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Signup from "./pages/Signup";
+import Signin from "./pages/Signin";
+import Home from "./pages/Home";
+import Projets from "./pages/Projets";
+import Dashboard from "./pages/Dashboard";
+import About from "./pages/About";
+import Header from "./components/Header";
+
+export default function App() {
+  return (
+    <BrowserRouter>
+      <Header />
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/signup" element={<Signup />} />
+        <Route path="/signin" element={<Signin />} />
+        <Route path="/projets" element={<Projets />} />
+        <Route path="/dashboard" element={<Dashboard />} />
+        <Route path="/about" element={<About />} />
+      </Routes>
+    </BrowserRouter>
+  );
+}
