ed2a87a9796674fb8aafac41c317a0a05beb84fa
hafayouness <155436001+hafayouness@users.noreply.github.com>
2024-07-16 17:08:53 +0200

    connect to the database

--- api/index.js
+++ api/index.js
@@ -1,6 +1,19 @@
 import express from "express";
-const app = express();
+import mongoose from "mongoose";
+import dotenv from "dotenv";
+dotenv.config();
 
+const app = express();
+const uri = process.env.MONGO_URI;
+mongoose
+  .connect(uri)
+  .then(() => {
+    console.log("Mongodb is conneted");
+  })
+  .catch((err) => {
+    console.log(err);
+  });
 app.listen(3000, () => {
   console.log("Server is running on part 3000 !!");
 });
+// TgoiiBqVMecjeWEW
