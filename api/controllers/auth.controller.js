a9898ec225671c5fbe659731e90c99746ad00322
hafayouness <155436001+hafayouness@users.noreply.github.com>
2024-08-02 21:54:27 +0200

    create api signin router

--- api/controllers/auth.controller.js
+++ api/controllers/auth.controller.js
@@ -1,6 +1,7 @@
 import User from "../models/user.model.js";
-
+import bcryptjs from "bcryptjs";
 import { errorHandler } from "../utils/error.js";
+import jwt from "jsonwebtoken";
 
 export const signup = async (req, res, next) => {
   const { username, email, password } = req.body;
@@ -11,21 +12,54 @@ export const signup = async (req, res, next) => {
   try {
     const existingUser = await User.findOne({ email });
     if (existingUser) {
-      // return next(errorHandler(409, "user already exists"));
-      return res.status(409).json({ message: "user already exists" });
+      return next(errorHandler(409, "user already exists"));
     }
-    // const saltRounds = 10;
-    // const hashPassword = await bcrypt.hash(password, saltRounds);
+    const saltRounds = 10;
+    const hashPassword = bcryptjs.hashSync(password, saltRounds);
     const newUser = new User({
       username,
       email,
-      password,
+      password: hashPassword,
     });
     await newUser.save();
     res.status(201).json({ message: "Signup successful" });
   } catch (error) {
     console.log(error);
-    // next(errorHandler(500, "Internal Server Error"));
-    res.status(500).json({ message: "Internal Server Error" });
+    next(errorHandler(500, "Internal Server Error"));
+  }
+};
+
+export const signin = async (req, res, next) => {
+  const { email, password } = req.body;
+  if (!email || !password || email === "" || password === "") {
+    next(errorHandler(400, "All fields are reaquired"));
+  }
+
+  try {
+    const validUser = await User.findOne({ email });
+    if (!validUser) {
+      return next(errorHandler(404, "User not found"));
+    }
+    const validPassword = bcryptjs.compareSync(password, validUser.password);
+    if (!validPassword) {
+      return next(errorHandler(400, "invalid password"));
+    }
+    const token = jwt.sign(
+      {
+        id: validUser._id,
+        isAdmin: validUser.isAdmin,
+      },
+      process.env.JWT_SECRET,
+      { expiresIn: "1y" }
+    );
+    const { password: pass, ...rest } = validUser._doc;
+    res
+      .status(200)
+      .cookie("access_token", token, {
+        httpOnly: true,
+      })
+      .json(rest);
+  } catch (err) {
+    next(err);
   }
 };
