cbf8d0d3177be5822f221d91b74b1c4cdcb44fe0
hafayouness <155436001+hafayouness@users.noreply.github.com>
2024-08-13 20:31:29 +0200

    complete dashSider of the dashboard

---
+++ client/src/components/DashSider.jsx
@@ -0,0 +1,41 @@
+import { Sidebar } from "flowbite-react";
+import React, { useEffect, useState } from "react";
+import { HiUser, HiArrowSmRight } from "react-icons/hi";
+import { Link, useLocation } from "react-router-dom";
+
+export default function DashSider() {
+  const location = useLocation();
+  const [tab, setTab] = useState("");
+  useEffect(() => {
+    const urlParams = new URLSearchParams(location.search);
+    const tabFormUrl = urlParams.get("tab");
+    if (tabFormUrl) {
+      setTab(tabFormUrl);
+      console.log(tabFormUrl);
+    }
+  }, [location.search]);
+  return (
+    <>
+      <Sidebar className="w-full md:w-56">
+        <Sidebar.Items>
+          <Sidebar.ItemGroup>
+            <Link to="/dashboard?tab=profile">
+              <Sidebar.Item
+                active={tab === "profile"}
+                icon={HiUser}
+                label={"User"}
+                labelColor="dark"
+              >
+                Profile
+              </Sidebar.Item>
+            </Link>
+
+            <Sidebar.Item icon={HiArrowSmRight} className="cursor-pointer">
+              Sign Out
+            </Sidebar.Item>
+          </Sidebar.ItemGroup>
+        </Sidebar.Items>
+      </Sidebar>
+    </>
+  );
+}
--- client/src/pages/Dashboard.jsx
+++ client/src/pages/Dashboard.jsx
@@ -1,7 +1,32 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
+import { useLocation } from "react-router-dom";
+import DashSider from "../components/DashSider";
+import DashProfile from "../components/DashProfile";
 
 function Dashboard() {
-  return <h1>Dashboard</h1>;
+  const location = useLocation();
+  const [tab, setTab] = useState("");
+  useEffect(() => {
+    const urlParams = new URLSearchParams(location.search);
+    const tabFormUrl = urlParams.get("tab");
+    if (tabFormUrl) {
+      setTab(tabFormUrl);
+      console.log(tabFormUrl);
+    }
+  }, [location.search]);
+  return (
+    <div className="dashboardCss flex flex-col md:flex-row">
+      <div className="md:w-56">
+        <DashSider />
+      </div>
+      {/* {tab === "profile" && <DashProfile />} */}
+      {tab === "profile" ? (
+        <DashProfile className="text-gray-800" />
+      ) : (
+        <p>No profile selected</p>
+      )}
+    </div>
+  );
 }
 
 export default Dashboard;
