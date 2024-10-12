a81d29b82a3649a525a37abaf979e880520b91fe
hafayouness <155436001+hafayouness@users.noreply.github.com>
2024-10-12 14:55:14 +0200

    add outdir in cite config

--- api/index.js
+++ api/index.js
@@ -2,20 +2,28 @@ import express from "express";
 import mongoose from "mongoose";
 import dotenv from "dotenv";
 import cors from "cors";
+import cookieParser from "cookie-parser";
+import path from "path";
+
 dotenv.config();
+
 import userRoutes from "../api/routes/user.route.js";
 import authRoutes from "../api/routes/auth.router.js";
 import PostRoutes from "../api/routes/post.router.js";
 import CommentRoutes from "../api/routes/comment.route.js";
-import cookieParser from "cookie-parser";
-import path from "path";
+
 const app = express();
+
+if (!process.env.MONGODB_URL) {
+  console.error("MONGODB_URL is not defined in .env");
+  process.exit(1);
+}
 
 app.use(express.json());
 app.use(cookieParser());
 app.use(
   cors({
-    origin: "http://localhost:5173",
+    origin: "http://localhost:5173", // Update for production
     credentials: true,
   })
 );
@@ -25,26 +33,26 @@ app.use((req, res, next) => {
 });
 
 const uri = process.env.MONGODB_URL;
+
 mongoose
   .connect(uri)
   .then(() => {
-    console.log("Mongodb is connected");
+    console.log("MongoDB is connected");
   })
   .catch((err) => {
-    console.log(err);
+    console.error("MongoDB connection error:", err);
   });
 
 const __dirname = path.resolve();
 
 // Routes
-
 app.use("/api/user", userRoutes);
 app.use("/api/auth", authRoutes);
 app.use("/api/post", PostRoutes);
 app.use("/api/comment", CommentRoutes);
 app.use(express.static(path.join(__dirname, "/client/dist")));
 app.get("*", (req, res) => {
-  res.sendFile(path.join(__dirname, "client", "dist", "index.html")); // Fix here
+  res.sendFile(path.join(__dirname, "client", "dist", "index.html"));
 });
 
 // Error handler
--- client/vite.config.js
+++ client/vite.config.js
@@ -1,9 +1,8 @@
-import { defineConfig } from "vite";
-import react from "@vitejs/plugin-react-swc";
-
-// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  build: {
+    outDir: "dist", // Ensures output goes to the 'client/dist' directory
+  },
   server: {
     proxy: {
       "/api": {
